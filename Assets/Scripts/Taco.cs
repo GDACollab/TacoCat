5e7c1dbc7945cc60c6b93fd17c55f4a9accc7150
TeiHek <lerandy1235@gmail.com>
2023-01-21 14:08:09 -0800

    Finish Taco Class

--- Assets/Scripts/Taco.cs
+++ Assets/Scripts/Taco.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Taco
 {
-    enum shellType { FLOUR, CORN, HARD_SHELL }
-    enum fillingType { BEEF, CHICKEN, BEANS }
-    enum toppingType { SAUCE_CHEESE, SHREDDED_CHEESE, SOUR_CREAM, NONE }
+    public enum shellType { FLOUR, CORN, HARD_SHELL }
+    public enum fillingType { BEEF, CHICKEN, BEANS }
+    public enum toppingType { SAUCE_CHEESE, SHREDDED_CHEESE, SOUR_CREAM, NONE }
 
-    private shellType shell;
-    private List<fillingType> filling;
-    private List<toppingType> toppings;
+    private shellType s_shell;
+    private List<fillingType> s_filling;
+    private List<toppingType> s_toppings;
+
+    Taco(shellType shell, List<fillingType> fillings, List<toppingType> toppings)
+    {
+        s_shell = shell;
+        s_filling = fillings;
+        s_toppings = toppings;
+    }
+
+    public void AddShell(shellType shell)
+    {
+        // Maybe don't replace the shell if there are already ingredients
+        s_shell = shell;
+    }
+
+    public void AddFilling(fillingType filling)
+    {
+        s_filling.Add(filling);
+    }
+
+    public void AddTopping(toppingType topping)
+    {
+        s_toppings.Add(topping);
+    }
 
     /// <summary>
     /// Returns a value between 0 and 1 based on how many ingredients match
@@ -27,7 +50,54 @@ public class Taco
         }
         totalIngredients++;
 
-        int shorterList = other.filling.Count < filling.Count ?  other.filling.Count : filling.Count;
-        //for (int i = 0; i <   )
+        int shorterFilling;
+        int longerFilling;
+        if (other.filling.Count < filling.Count)
+        {
+            shorterFilling = other.filling.Count;
+            longerFilling = filling.Count;
+        }
+        else
+        {
+            shorterFilling = filling.Count;
+            longerFilling = other.filling.Count;
+        }
+
+        for (int i = 0; i < shorterFilling; i++)
+        {
+            if (filling[i] == other.filling[i])
+            {
+                matchingIngredients++;
+            }
+            totalIngredients++;
+        }
+        // Catch any ingredient mismatch if one list is shorter than the other
+        totalIngredients += longerFilling - shorterFilling;
+
+        int shorterTopping;
+        int longerTopping;
+        if (other.toppings.Count < toppings.Count)
+        {
+            shorterTopping = other.toppings.Count;
+            longerTopping = toppings.Count;
+        }
+        else
+        {
+            shorterTopping = toppings.Count;
+            longerTopping = other.toppings.Count;
+        }
+
+        for (int i = 0; i < shorterTopping; i++)
+        {
+            if (toppings[i] == other.toppings[i])
+            {
+                matchingIngredients++;
+            }
+            totalIngredients++;
+        }
+        // Catch any ingredient mismatch if one list is shorter than the other
+        totalIngredients += longerTopping - shorterTopping;
+
+        return (float) matchingIngredients / (float) totalIngredients;
     }
 }
