9678ec129867a7ada79d95448c61b0067b6c42a9
AkBalakr <akash.balakrish@gmail.com>
2023-01-21 17:23:50 -0800

    Fuel Text

--- Assets/Scenes/Vehicle.cs
+++ Assets/Scenes/Vehicle.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/* 
+    GetFuel()
+*/
 public class Vehicle : MonoBehaviour
 {
     bool jumpPress;
     bool gas;
     bool reverse;
+    public float acceleration = 10.0f;
+    public int fuel = 500;
     private float thurst = 500.0f;
     private float gravity = 50.0f;
     Rigidbody2D rb_vehicle;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,15 +33,20 @@ public class Vehicle : MonoBehaviour
         }
         if(gas) {
             print("right key pressed: VRoom");
-            rb_vehicle.velocity = new Vector2(10,0);
+            rb_vehicle.velocity += new Vector2(acceleration,0);
+            fuel--;
         }
         if(reverse) {
             print("left key pressed: un-VRoom");
-            rb_vehicle.velocity = new Vector2(-10,0);
+            rb_vehicle.velocity += new Vector2(-acceleration,0);
+            fuel--;
         }
     }
     void FixedUpdate() {
-        //rb_vehicle.velocity = new Vector2(10,0);
         rb_vehicle.AddForce(Vector2.down * gravity * rb_vehicle.mass);
+    }
+
+    public int GetFuel() {
+        return fuel;
     }
 }
