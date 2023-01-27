573f9e5bbef32501e7de9c496c3bb469856350fc
itspebbleboy <itspebbleboy@gmail.com>
2023-01-26 19:55:32 -0800

    Update Taco.cs
    updates to Taco class (mostly psuedo code for taco checking)

--- Assets/Scripts/Taco.cs
+++ Assets/Scripts/Taco.cs
@@ -4,94 +4,108 @@ using UnityEngine;
 
 public class Taco
 {
+    public enum scoreType{PERFECT, GOOD, OKAY, FAILED}
     public enum ingredientType {FISH, SOUR_CREAM, PICO_DE_GALLO, CABBAGE, SLICED_JALAPENOS}
+    public enum customerType{CAT, DOG, FISH}
     private List<ingredientType> s_ingredients;
+    private scoreType s_score;
+    private customerType s_customer;
+    
 
-
-    public Taco(List<ingredientType> ingredients)
+    public Taco(List<ingredientType> ingredients, customerType customer)
     {
         s_ingredients = ingredients;
+        s_customer= customer;
+        s_score= scoreType.PERFECT;
     }
-
+    public void setScore(scoreType incomingScore){
+        s_score=incomingScore;
+    }
     /// <summary>
     /// Returns a value between 0 and 1 based on how many ingredients match
     /// </summary>
-    /// <param name="other"></param>
+    /// <param name="currTaco"></param>
     /// <returns>A value between 0 and 1, the percentage of taco ingredient match</returns>
 
     //CHECKING TACO PSUEDO CODE
     /*
         for e/a taco
-        go through list subtracting from example taco
-        if curr ingredient doesn't match example ingredient 
-            if topping matches customer type then add dialogue or something
-            denote to "Good Taco" (reset perfect counter)
-            check to make sure ingredients match even if order is wrong
-        if ingredient is in taco, that doesn't match example taco, 
-            denote to "Okay Taco" 
-            add one to ingredientMisMatchCounter
-        if ingredientMisMatchCounter gets increases to 2, denote to "Failed Taco" STOP CHECKING
-        if done iterating through taco, check if exampleTaco has any elements, if it has 1 extra,
-            denote taco to "Okay Taco"
-        if more extra
-            denote taco to "Failed Taco"
+        if customer is a fish, if taco contains fish
+            denote to "Failed Taco" cool dialogue,
+        PERFECT TACO:
+            go through lists subtracting from taco and example taco
+            if curr ingredient doesn't match example ingredient
+                denote to "Good Taco"
+        GOOD TACO:
+            check to make sure ingredients match even if order is wrong subtract from lists
+            if ingredient is in taco, that doesn't match example taco, subtract from taco list
+                add one to ingredientMisMatchCounter
+        WHEN done iterating through taco, 
+            check if exampleTaco has any elements, add # of extra elements to ingredientMisMatchCounter
+        if ingredientMisMatchCounter == 1 -> "Okay Taco"
+        if ingredientMisMatchCounter > 1 -> "Failed Taco"
+
+        if taco isn't perfect, reset perfectCounter
+        else increment perfectCounter
+        is perfectCounter % 3 display combo message, increment comboCounter.
     */
-    public float CheckTaco(Taco other)
+    public float CheckTaco(Taco currTaco)
     {
-        int totalIngredients = 0;
-        int matchingIngredients = 0;
-        if (s_shell == other.s_shell)
-        {
-            matchingIngredients++;
+        int ingredientMisMatchCounter = 0; // when == 1 -> "Okay Taco", when >=2 -> "Failed Taco"
+        if(s_customer=FISH){
+            currTaco.setScore(FAILED);
+            //currTaco.angeredFish = true; ???
+        }else{
+            
         }
         totalIngredients++;
 
         int shorterFilling;
         int longerFilling;
-        if (other.s_filling.Count < s_filling.Count)
+        if (currTaco.s_filling.Count < s_filling.Count)
         {
-            shorterFilling = other.s_filling.Count;
+            shorterFilling = currTaco.s_filling.Count;
             longerFilling = s_filling.Count;
         }
         else
         {
             shorterFilling = s_filling.Count;
-            longerFilling = other.s_filling.Count;
+            longerFilling = currTaco.s_filling.Count;
         }
 
         for (int i = 0; i < shorterFilling; i++)
         {
-            if (s_filling[i] == other.s_filling[i])
+            if (s_filling[i] == currTaco.s_filling[i])
             {
                 matchingIngredients++;
             }
             totalIngredients++;
         }
-        // Catch any ingredient mismatch if one list is shorter than the other
+        // Catch any ingredient mismatch if one list is shorter than the currTaco
         totalIngredients += longerFilling - shorterFilling;
 
         int shorterTopping;
         int longerTopping;
-        if (other.s_toppings.Count < s_toppings.Count)
+        if (currTaco.s_toppings.Count < s_toppings.Count)
         {
-            shorterTopping = other.s_toppings.Count;
+            shorterTopping = currTaco.s_toppings.Count;
             longerTopping = s_toppings.Count;
         }
         else
         {
             shorterTopping = s_toppings.Count;
-            longerTopping = other.s_toppings.Count;
+            longerTopping = currTaco.s_toppings.Count;
         }
 
         for (int i = 0; i < shorterTopping; i++)
         {
-            if (s_toppings[i] == other.s_toppings[i])
+            if (s_toppings[i] == currTaco.s_toppings[i])
             {
                 matchingIngredients++;
             }
             totalIngredients++;
         }
-        // Catch any ingredient mismatch if one list is shorter than the other
+        // Catch any ingredient mismatch if one list is shorter than the currTaco
         totalIngredients += longerTopping - shorterTopping;
 
         return (float) matchingIngredients / (float) totalIngredients;
