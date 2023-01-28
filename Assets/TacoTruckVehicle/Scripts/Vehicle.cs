3e9a4e841118166c6d1fcf49de924bbf917f942c
Sky Casey <theocasey11@gmail.com>
2023-01-27 18:17:01 -0800

    updating truck move physics

--- Assets/TacoTruckVehicle/Scripts/Vehicle.cs
+++ Assets/TacoTruckVehicle/Scripts/Vehicle.cs
@@ -12,6 +12,12 @@ public class Vehicle : MonoBehaviour
     public GameObject frontWheel;
     public GameObject backWheel;
 
+
+    [Space(10)]
+    public LayerMask groundLayer;
+    public List<Collider2D> groundColliderList;
+    public float groundColliderSize = 15;
+
     [Header("States")]
     public bool isGrounded;
     public bool inAir;
@@ -20,8 +26,8 @@ public class Vehicle : MonoBehaviour
 
     [Header("Forces")]
     public Vector2 startingVelocity; // initial velocity
-    public Vector2 gasForce; // input based force on truck
-    public Vector2 jumpForce; // jump force
+    public Vector2 inAirForce; // input based force on truck
+    public Vector2 groundedForce; // input based force on truck
 
     [Header("Inputs")]
     public KeyCode gas;
@@ -51,44 +57,48 @@ public class Vehicle : MonoBehaviour
     }
 
     void FixedUpdate() {
-        // constant gravity
+
+
+        // << CONSTANT GRAVITY >>
         rb_vehicle.AddForce(Vector2.down * gravity * rb_vehicle.mass);
+
+        // << CHECK GROUND >>
+        Collider2D[] groundColliders = Physics2D.OverlapCircleAll(transform.position, groundColliderSize, groundLayer);
+        groundColliderList = new List<Collider2D>(groundColliders);
+
+        // set bool
+        if (groundColliderList.Count > 0) { isGrounded = true; inAir = false; }
+        else { isGrounded = false; inAir = true; }
 
         // << GAS STATE >>
         if (gasPressed && fuelAmount > 0)
         {
-            rb_vehicle.AddForce(gasForce * rb_vehicle.mass);
+            // in air force
+            if (!isGrounded)
+            {
+                Debug.Log("airForce");
+                rb_vehicle.AddForce(inAirForce * rb_vehicle.mass);
+            }
+            // on ground force
+            else
+            {
+                Debug.Log("groundForce");
+                rb_vehicle.AddForce(groundedForce * rb_vehicle.mass);
+
+            }
 
             fuelAmount--;
         }
+
     }
 
     public void Inputs()
     {
         gasPressed = Input.GetKey(gas);
 
-        jumpState = Input.GetKeyUp(KeyCode.Space);
-
-        // << JUMP STATE >>
-        // needs to be in update because input is single-frame based
-        if (jumpState)
-        {
-            Debug.Log("Jump");
-            rb_vehicle.AddForce(jumpForce * rb_vehicle.mass, ForceMode2D.Impulse);
-        }
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.tag == "Terrain") {
-            isGrounded = true;
-        }
-    }
 
-    void OnCollisionExit2D(Collision2D collision) {
-        if(collision.gameObject.tag == "Terrain") {
-            isGrounded = false;
-        }
-    }
 
     public int GetFuel() {
         return fuelAmount;
@@ -102,6 +112,8 @@ public class Vehicle : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(transform.position, groundColliderSize);
 
         // draw ray to show current velocity of rigidbody
         if (rb_vehicle != null)
