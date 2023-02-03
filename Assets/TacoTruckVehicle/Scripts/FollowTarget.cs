4ce55bd56ff0d901c0a267d68022627a0c2b344b
Theo Casey <theocasey11@gmail.com>
2023-02-02 16:13:28 -0800

    skybox owo

--- Assets/TacoTruckVehicle/Scripts/FollowTarget.cs
+++ Assets/TacoTruckVehicle/Scripts/FollowTarget.cs
@@ -12,27 +12,14 @@ public class FollowTarget : MonoBehaviour
     public float camSpeed = 0.2f;
    
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         
         //Camera shares x,y,z + offset(x,y,z)
         if(target) {
-            transform.position = Vector3.Lerp(transform.position, Round(target.position + offset, 0), camSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, target.position + offset, camSpeed * Time.deltaTime);
             //transform.position = target.position + offset;
 
         }
-    }
-
-    public static Vector3 Round(Vector3 vector3, int decimalPlaces = 2)
-    {
-        float multiplier = 1;
-        for (int i = 0; i < decimalPlaces; i++)
-        {
-            multiplier *= 10f;
-        }
-        return new Vector3(
-            Mathf.Round(vector3.x * multiplier) / multiplier,
-            Mathf.Round(vector3.y * multiplier) / multiplier,
-            Mathf.Round(vector3.z * multiplier) / multiplier);
     }
 }
