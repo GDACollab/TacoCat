a978854923849dddc3781c78bf4aa77a56c72c75
Jorel Huerto <jjhue29@gmail.com>
2023-02-04 20:53:49 -0800

    some implementation of landmark and gas station scripts

--- Assets/Location/PointOfInterest/PointsOfInterest/Gas Station.cs
+++ Assets/Location/PointOfInterest/PointsOfInterest/Gas Station.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class GasStation : PointOfInterest
 {
+    public Rigidbody2D rb_gasStation;//dunno if we need this cause idk about Rigibody2D that much
     public Vector3 location;
     // Start is called before the first frame update
     void Start()
     {
-        
+        location = _location;
     }
 
     // Update is called once per frame
--- Assets/Location/PointOfInterest/PointsOfInterest/Landmark.cs
+++ Assets/Location/PointOfInterest/PointsOfInterest/Landmark.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Landmark : PointOfInterest
-{ 
+{
+    public Rigidbody2D rb_landmark;//dunno if we need this cause idk about Rigibody2D that much
     public Vector3 location;
     // Start is called before the first frame update
     void Start()
     {
-        
+        location = _location;
     }
 
     // Update is called once per frame
