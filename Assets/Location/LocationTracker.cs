df4c0600507ba42053428dff24a50b639c0e87a4
Jorel Huerto <jjhue29@gmail.com>
2023-02-04 20:16:14 -0800

    Updated Location Tracker to track vehicle position, as well as inspector info touch up

--- Assets/Location/LocationTracker.cs
+++ Assets/Location/LocationTracker.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class LocationTracker : MonoBehaviour
 {
+    [Header("Player Vehicle")]
+    public GameObject playerVehicle;
     // LocationTracker has all the PointsOfInterests of the current scene
+    [Header("List of Landmarks")]
     public List<PointOfInterest> allPointsOfInterest = new List<PointOfInterest>();
+
+    [Header("Vehicle Location")]
+    private Vehicle vehicle = null;
+    public Vector3 vehiclePosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vehicle = playerVehicle.GetComponent<Vehicle>();
+        vehiclePosition = vehicle.GetPosition();
+        Debug.Log(vehiclePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vehiclePosition = vehicle.GetPosition();
+        //Debug.Log(vehiclePosition);
     }
 }
