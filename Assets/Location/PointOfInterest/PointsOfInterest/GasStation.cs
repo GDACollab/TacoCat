99c97f5f4274d52ce9ad979b5dd7b30c47673454
Jorel Huerto <jjhue29@gmail.com>
2023-02-05 19:09:15 -0800

    Gas Station and Landmark prefabs

--- Assets/Location/PointOfInterest/PointsOfInterest/GasStation.cs
+++ Assets/Location/PointOfInterest/PointsOfInterest/GasStation.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class GasStation : PointOfInterest
 {
+    
     public Rigidbody2D rb_gasStation;//dunno if we need this cause idk about Rigibody2D that much
-    public Vector3 location;
-    // Start is called before the first frame update
-    void Start()
-    {
-        location = _location;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Location/PointOfInterest/PointsOfInterest/Landmark.cs
+++ Assets/Location/PointOfInterest/PointsOfInterest/Landmark.cs
@@ -5,16 +5,4 @@ using UnityEngine;
 public class Landmark : PointOfInterest
 {
     public Rigidbody2D rb_landmark;//dunno if we need this cause idk about Rigibody2D that much
-    public Vector3 location;
-    // Start is called before the first frame update
-    void Start()
-    {
-        location = _location;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
