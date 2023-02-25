4cae0b03e1c8b6bb48643197aef845ac70a49523
Sky Casey <theocasey11@gmail.com>
2023-02-25 12:50:19 -0800

    init

---
+++ Assets/Cutscene/CutsceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public TextMeshProUGUI phoneText;
+
+    public List<string> phone_texts;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
