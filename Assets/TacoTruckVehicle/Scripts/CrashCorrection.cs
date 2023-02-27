b77cccd31f308ce1b8899f5796f085f41d3a8b59
Theo Casey <theocasey11@gmail.com>
2023-02-26 19:24:47 -0800

    Collision Correction done

--- Assets/TacoTruckVehicle/Scripts/CrashCorrection.cs
+++ Assets/TacoTruckVehicle/Scripts/CrashCorrection.cs
@@ -1,19 +1,27 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CrashCorrection : MonoBehaviour
 {
     Vehicle vehicle;
     Rigidbody2D rb;
+    public BoxCollider2D collisionTrigger;
 
-    public Vector3 resetRotation;
+    public float resetAngle = 0;
+    public float rotationSpeed = 2;
     public GameObject collisionEffect;
+
+    public bool isCorrecting;
 
     private void Awake()
     {
         vehicle = GetComponentInParent<Vehicle>();
         rb = GetComponentInParent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        if (isCorrecting) { RotateToResetAngle(); }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,11 +37,53 @@ public class CrashCorrection : MonoBehaviour
     public IEnumerator CorrectCrash()
     {
         rb.velocity = Vector3.zero;
-        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
 
-        vehicle.transform.rotation = Quaternion.Euler(resetRotation);
+        collisionTrigger.enabled = false;
 
-        yield return null;
+        vehicle.transform.position += Vector3.up * 10;
+
+        isCorrecting = true;
+
+        yield return new WaitUntil(() => IsTruckUpright(0.2f));
+
+
+        rb.constraints = RigidbodyConstraints2D.None;
+
+        collisionTrigger.enabled = true;
+
+        isCorrecting = false;
+
+    }
+
+    public void RotateToResetAngle()
+    {
+        // Get the current rotation of the object
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+
+        // Set the z rotation to the target rotation
+        currentRotation.z = Mathf.MoveTowardsAngle(currentRotation.z, resetAngle, rotationSpeed * Time.deltaTime);
+
+        // Apply the new rotation to the object
+        transform.rotation = Quaternion.Euler(currentRotation);
+    }
+
+    public bool IsTruckUpright(float buffer)
+    {
+        // Get the current rotation of the object
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        float difference = Mathf.Abs(currentRotation.z - resetAngle);
+
+        Debug.Log("difference " + difference);
+
+        if (difference < buffer)
+        {
+            return true;
+        }
+
+        Debug.Log("Rotation Difference " + currentRotation.z);
+
+        return false;
     }
 
 }
