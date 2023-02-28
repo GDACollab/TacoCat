6ece902bfb07fbf834ab75149768b07dfbedd9d9
Theo Casey <theocasey11@gmail.com>
2023-02-28 09:00:38 -0800

    set upt taco game manager

--- Assets/SceneManagement/GameManager.cs
+++ Assets/SceneManagement/GameManager.cs
@@ -10,10 +10,25 @@ public class GameManager : MonoBehaviour
     public int drivingIndex = 2;
     public int cutscene = 3;
 
+    TacoMakingGameManager tacoGameManager;
+    // DrivingGameManager drivingGameManager;
 
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Update()
+    {
+        // << TACO GAME MANAGER >>
+        if (tacoGameManager == null)
+        {
+            tacoGameManager = GameObject.FindGameObjectWithTag("TacoGameManager").GetComponent<TacoMakingGameManager>();
+        }
+        else
+        {
+            // check if all customers submitted , if so move to driving with gas amount
+        }
     }
 
     public void LoadMenu()
