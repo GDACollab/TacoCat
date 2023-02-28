352059c76aa5ea6c7e70b9be54e8c5ad3395af40
BenitoG123 <bgravert@hotmail.com>
2023-02-27 21:22:58 -0800

    created variables and printed them to the TMpro
    First Attempt at printing out the list onto the TMpro object.
    
    Bug: 'hello' is always printed first. I don't know where it comes from
    
    added 2 public variables and added them to phone_texts:
    first_line
    second_line
    
    created a foreach loop to print each line on TMPro

--- Assets/Cutscene/CutsceneManager.cs
+++ Assets/Cutscene/CutsceneManager.cs
@@ -9,11 +9,24 @@ public class CutsceneManager : MonoBehaviour
 
     public List<string> phone_texts;
 
+    public string first_line = "This should print first.";
+    public string second_line = "This should print second.";
 
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        phone_texts.Add(first_line);
+        phone_texts.Add(second_line);
+
+        // clear phoneText
+        phoneText.text = string.Empty;
+
+        //Add each element from phone_texts to phoneText
+        foreach (string line in phone_texts) {
+            phoneText.text += line + "\n";
+        }
         
     }
 
