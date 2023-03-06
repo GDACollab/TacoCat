ff4c24dfb3b32edcbe882fe583ebec61ab4f2c2f
itspebbleboy <itspebbleboy@gmail.com>
2023-03-06 14:22:19 -0800

    Update TacoAudioManager.cs

--- Assets/TacoAudioManager.cs
+++ Assets/TacoAudioManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class TacoAudioManager : MonoBehaviour
 {
+    CustomerManager customerManager;
+    public TacoMakingGameManager tacoGameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        customerManager = tacoGameManager.customerManager;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (customerManager.currCustomer != null)
+        {
+            //OrderAudio(customerManager.currCustomer.order );
+        }
+    }
+
+    public void OrderAudio(){
     }
 }
