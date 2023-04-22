1747503b1c70de2f980aa7bab4289a4a6dcae5d3
Tyler K <tyly04@gmail.com>
2023-04-22 13:22:13 -0700

    Quick loading screen setup

--- Assets/SceneManagement/GameManager.cs
+++ Assets/SceneManagement/GameManager.cs
@@ -4,10 +4,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum currScene { MENU , INTRO_CUTSCENE , TACO_MAKING , DRIVING }
+public enum currScene { MENU, INTRO_CUTSCENE, TACO_MAKING, DRIVING }
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
     public bool isLoadingScene;
 
@@ -36,59 +35,42 @@ public class GameManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void Update()
-    {
-        
+    public void Update() {
+
         // << TACO GAME MANAGER >>
-        if (tacoGameManager == null)
-        {
-            try
-            {
+        if (tacoGameManager == null) {
+            try {
                 tacoGameManager = GameObject.FindGameObjectWithTag("TacoGameManager").GetComponent<TacoMakingGameManager>();
-            }
-            catch{ }
-        }
-        else
-        {
+            } catch { }
+        } else {
             // check if all customers submitted , if so move to driving with gas amount
-            if (tacoGameManager.endOfGame && !isLoadingScene)
-            {
+            if (tacoGameManager.endOfGame && !isLoadingScene) {
                 LoadDrivingScene();
             }
         }
 
 
         // << DRIVING GAME MANAGER >>
-        if (drivingGameManager == null)
-        {
-            try
-            {
+        if (drivingGameManager == null) {
+            try {
                 drivingGameManager = GameObject.FindGameObjectWithTag("DrivingGameManager").GetComponent<DrivingGameManager>();
-            }
-            catch { }
-        }
-        else {
+            } catch { }
+        } else {
 
-            if (drivingGameManager.endOfGame && !isLoadingScene)
-            {
+            if (drivingGameManager.endOfGame && !isLoadingScene) {
                 LoadTacoMakingScene();
             }
 
         }
 
         // << CUTSCENE MANAGER >>
-        if (cutsceneManager == null)
-        {
-            try
-            {
+        if (cutsceneManager == null) {
+            try {
                 cutsceneManager = GameObject.FindGameObjectWithTag("CutsceneManager").GetComponent<CutsceneManager>();
-            }
-            catch { }
-        }
-        else { 
+            } catch { }
+        } else {
 
-            if (cutsceneManager.endOfCutscene && !isLoadingScene)
-            {
+            if (cutsceneManager.endOfCutscene && !isLoadingScene) {
                 LoadTacoMakingScene();
             }
         }
@@ -104,40 +86,37 @@ public class GameManager : MonoBehaviour
         }*/
     }
 
-    public void LoadMenu()
-    {
+    public void LoadMenu() {
         SceneManager.LoadScene(menuIndex);
         audioManager.PlaySong(menuIndex + "Music");
     }
 
-    public void LoadTacoMakingScene()
-    {
+    public void LoadTacoMakingScene() {
 
         StartCoroutine(LoadingCoroutine(tacoMakingIndex));
         audioManager.PlaySong("TacoMusic");
 
     }
 
-    public void LoadDrivingScene()
-    {
+    public void LoadDrivingScene() {
         StartCoroutine(LoadingCoroutine(drivingIndex));
         audioManager.PlaySong("DrivingMusic");
     }
 
-    public void LoadRandDrivingScene()
-    {
+    public void LoadRandDrivingScene() {
         StartCoroutine(LoadingCoroutine(randDriveIndex));
         audioManager.PlaySong("DrivingMusic");
     }
 
-    public void LoadNightDrivingScene()
-    {
+    public void LoadNightDrivingScene() {
         StartCoroutine(LoadingCoroutine(nightDriveIndex));
         audioManager.PlaySong("DrivingMusic");
     }
 
-    IEnumerator LoadingCoroutine(string sceneIndex)
-    {
+    [HideInInspector]
+    public float loadProgress;
+
+    IEnumerator LoadingCoroutine(string sceneIndex) {
         isLoadingScene = true;
         yield return null;
         currentScene = menuIndex;
@@ -151,26 +130,20 @@ public class GameManager : MonoBehaviour
         Debug.Log("Pro :" + newScene.progress);
 
         //When the load is still in progress, output the Text and progress bar
-        while (!newScene.isDone)
-        {
-            //Output the current progress
-            Debug.Log("Loading progress: " + (newScene.progress * 100) + "%");
+        while (!newScene.isDone) {
+            loadProgress = newScene.progress;
 
-            // Check if the load has finished
-            if (newScene.progress >= 0.9f)
-            {
+            if (newScene.progress >= 0.9f) {
                 newScene.allowSceneActivation = true;
             }
-
-            yield return null;
+            yield return new WaitForEndOfFrame();
         }
 
         SceneManager.UnloadSceneAsync(loadingSceneIndex);
         isLoadingScene = false;
     }
 
-    public void LoadCutscene()
-    {
+    public void LoadCutscene() {
         SceneManager.LoadScene(cutsceneIndex);
         audioManager.PlaySong("StoryMusic");
     }
