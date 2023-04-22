fc88a76ce29ce49ad9539ac00f3f6274516535ff
Tyler K <tyly04@gmail.com>
2023-04-22 16:07:36 -0700

    Night drive is back in

--- Assets/Driving/Vehicle/Scripts/DrivingGameManager.cs
+++ Assets/Driving/Vehicle/Scripts/DrivingGameManager.cs
@@ -37,7 +37,6 @@ public class DrivingGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         // << INIT SCENE >>
         if (groundGeneration.generationFinished)
         {
