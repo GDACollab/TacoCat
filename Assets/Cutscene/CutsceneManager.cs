f14793ef1651e34ffc30ba0bf1c77a5267e274cf
BenitoG123 <bgravert@hotmail.com>
2023-05-02 17:32:54 -0700

    preparing for bubble prefab
    I still need to figure out how the prefabs are going to scroll
    
    I commented out a lot of code so it doesn't print out what Jamie says
    
    I added it to say "Jamie is typing..." during Jamie's message delay instead
    
    You can still see Alex typing out their text, but it will delete once they hit send, so later it should seem like the text was sent.

--- Assets/Cutscene/CutsceneManager.cs
+++ Assets/Cutscene/CutsceneManager.cs
@@ -13,8 +13,10 @@ public class CutsceneManager : MonoBehaviour
     public GameObject alexBubble;
     public GameObject jamieBubble;
 
+    private float startingPosition;
+
     [Range(0.0f, 10.0f)]
-    private float scroll;
+    public float scroll;
 
     [Tooltip("[WIP] Max charcters per line.\nWon't move the whole word to the next line currently")]
     public int characterLimit;
@@ -71,10 +73,11 @@ public class CutsceneManager : MonoBehaviour
         phoneText.text = string.Empty;
         
         counter = 0;
+        startingPosition = 0;
 
         //testing
-        Instantiate(alexBubble, alexBubble.transform.position, transform.rotation);
-        Instantiate(jamieBubble, jamieBubble.transform.position, transform.rotation);
+        //Instantiate(alexBubble, alexBubble.transform.position, transform.rotation);
+        //Instantiate(jamieBubble, jamieBubble.transform.position, transform.rotation);
 
         StartCoroutine(begin());
 
@@ -89,7 +92,7 @@ public class CutsceneManager : MonoBehaviour
                 //Alex
 
 
-                phoneText.text += "Alex: ";
+                //phoneText.text += "Alex: ";
                 yield return StartCoroutine(Typeline(a.texts));
                 
 
@@ -101,12 +104,14 @@ public class CutsceneManager : MonoBehaviour
                 //slight pause so Jamie doesn't respond instantly
                 yield return new WaitForSeconds(messageDelayJamie);
 
-                phoneText.text += "Jamie: ";
+                //scroll all texts
+                //increase all y values of instances by scroll
+
+                phoneText.text += "Jamie is typing...";
                 yield return StartCoroutine(PrintText(a.texts));
+                phoneText.text = "";
             }
 
-            //scroll all texts
-            //increase all y values of instances by scroll
         }
 
         endOfCutscene = true;
@@ -119,13 +124,14 @@ public class CutsceneManager : MonoBehaviour
         //Add each element from phone_texts to phoneText
 
         foreach (string l in characterText)
-            {
-                //insert instance of jamiebubble text += l + "\n"
-                phoneText.text += l + "\n";
-                //Instantiate(jamieBubble);
+        {
+            //phoneText.text += l + "\n";
 
-                yield return new WaitForSeconds(messageDelayJamie);
-            }
+            //insert instance of jamiebubble text += l + "\n"
+            GameObject JBubble = Instantiate(jamieBubble, new Vector3(-5,startingPosition,0), Quaternion.identity);
+            startingPosition += scroll;
+            yield return new WaitForSeconds(messageDelayJamie);
+        }
             
 
     }
@@ -137,27 +143,33 @@ public class CutsceneManager : MonoBehaviour
         foreach (string s in l)
         {
            
-           
-                //Add each element from phone_texts to phoneText
-                foreach (char c in s.ToCharArray())
+            //Add each element from phone_texts to phoneText
+            foreach (char c in s.ToCharArray())
+            {
+                //insert instance of alexbubble text += c
+                phoneText.text += c;
+
+                counter++;
+
+                yield return new WaitForSeconds(textSpeedAlex);
+                if (counter == characterLimit)
                 {
-                    //insert instance of alexbubble text += c
-                    phoneText.text += c;
-
-                    counter++;
-
-                    yield return new WaitForSeconds(textSpeedAlex);
-                    if (counter == characterLimit)
-                    {
-                        //insert instance of alexbubble text += "\n"
-                        phoneText.text += "\n";
-                        counter = 0;
-                    }
-
+                    //insert instance of alexbubble text += "\n"
+                    phoneText.text += "\n";
+                    counter = 0;
                 }
-                //insert instance of alexbubble text += "\n"
-                phoneText.text += "\n";
-                yield return new WaitForSeconds(messageDelayAlex);
+
+            }
+            //insert instance of alexbubble text += "\n"
+
+            //phoneText.text += "\n";
+            phoneText.text = "";
+
+            //scroll all texts
+            //increase all y values of instances by scroll
+            GameObject ABubble = Instantiate(alexBubble, new Vector3(5, startingPosition, 0), Quaternion.identity);
+            startingPosition += scroll;
+            yield return new WaitForSeconds(messageDelayAlex);
             
         }
     }
