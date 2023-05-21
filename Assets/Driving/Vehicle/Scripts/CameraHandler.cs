761114311297920d8a212b4a673051591f08da39
Theo Casey <theocasey11@gmail.com>
2023-05-20 17:35:33 -0700

    added Gizmos, and restructured the way the offset is calculated with the lerp

--- Assets/Driving/Vehicle/Scripts/CameraHandler.cs
+++ Assets/Driving/Vehicle/Scripts/CameraHandler.cs
@@ -11,7 +11,8 @@ public class CameraHandler : MonoBehaviour
     public GameObject ground;
 
     [Space(10)]
-    public float camSpeed = 0.2f;
+    public float camSpeed = 5f;
+    public Vector3 offsetAdjustSpeed = new Vector3(3, 3, 3);
 
     [Header("Camera Shake")]
     [Range(0, 1)]
@@ -30,13 +31,39 @@ public class CameraHandler : MonoBehaviour
     public Vector2 yPosRange = new Vector2(0, -200);
     public Vector2 zPosRange = new Vector2(-200, -500); // the range of z positions the camera should adjust between
 
-    public Vector3 currOffset;
+
+
+    [Header("Camera Generation-Based Offset")]
+    public Vector3 currCamOffset;
+
+    // Each value = 1/6 of the distance from the ceenter to the screen edge, + is to the left and - is to the right
+    public float cameraSixthOffset = 2;
+
+    // Modified fixedUpdate() to work with the Zero point code
+
+    float currZeroPos;
+
+
+    Vector3 currApoint;
+    Vector3 currBpoint;
+
+
+    // slope/constant values used in equation
+    float slope;
+    float constant;
+    // Used for determining if new bezier points should be added to the list
+    Vector3 lastCurvePoint = new Vector3(-99999999, 99999999, 0); //Tracks lowest Point from the last curve to judge what points should get added
+
+    List<Vector3> camBezierPoints = new List<Vector3>(); //List of points used to determine where the 'zero' point should be
+    public int bezierPointsListTracker = 0; //Current positioning in the list of points
+
 
     private void Start()
     {
         vehicleRb = vehicle.GetComponent<Rigidbody2D>();
     }
 
+    /*
     // Update is called once per frame
     void OldUpdate()
     {
@@ -61,6 +88,7 @@ public class CameraHandler : MonoBehaviour
             
         }
     }
+    */
 
 
     public IEnumerator Shake(float duration, float magnitude)
@@ -80,46 +108,34 @@ public class CameraHandler : MonoBehaviour
 
 
 
-
-
-
-    //Camera Zero stuff
-
-    //List of variables used to determine points
-    // x/y values of current points
-    float a_x_pos;
-    float a_y_pos;
-    float b_x_pos;
-    float b_y_pos;
-    // slope/constant values used in equation
-    float slope;
-    float constant;
-    // Used for determining if new bezier points should be added to the list
-    Vector3 lastCurvePoint = new Vector3(-99999999, 99999999, 0); //Tracks lowest Point from the last curve to judge what points should get added
-
-    List<Vector3> bezierPoints = new List<Vector3>(); //List of points used to determine where the 'zero' point should be
-    int bezierPointsListTracker = 0; //Current positioning in the list of points
-
     //Called in the bezier generator, adds the P_1/P_2 point to the bezierPoints list
     //(To do so, whenever a P_1/2 point is made, run this function with that point before moving onto the next point)
     public void addToBezierPoints(Vector3 point)
     {
+        /*
         if (lastCurvePoint.y > point.y && lastCurvePoint.x != point.x) //y check to prevent unwanted height increases, x to prevent duplicates
         {
-            bezierPoints.Add(point);
+            camBezierPoints.Add(point);
         }
+        */
+
+        if (lastCurvePoint.x != point.x) //y check to prevent unwanted height increases, x to prevent duplicates
+        {
+            camBezierPoints.Add(point);
+        }
+
         lastCurvePoint = point;
     }
 
     //Determines the numbers used for making the equation
     public void MakeEquation(Vector3 A_pos, Vector3 B_pos)
     {
-        a_x_pos = A_pos.x;
-        a_y_pos = A_pos.y;
-        b_x_pos = B_pos.x;
-        b_y_pos = B_pos.y;
-        slope = (b_y_pos - a_y_pos) / (b_x_pos - a_x_pos);
-        constant = (b_y_pos - (slope * (-1))); //-1 = cos(pi)
+        currApoint = A_pos;
+        currBpoint = B_pos;
+
+        slope = (currBpoint.y - currApoint.y) / (currBpoint.x - currApoint.x);
+
+        constant = (currBpoint.y - (slope * (-1))); //-1 = cos(pi)
     }
 
     //Runs the equation to determine that determines the current 'zero' point
@@ -127,26 +143,25 @@ public class CameraHandler : MonoBehaviour
     {
         //Check if car is within range of list
         if (bezierPointsListTracker < 0)
-        {//If not, the zero is the y of the closest point
-            Debug.Log("Calculate A");
-            return bezierPoints[0].y;
-        }
-        else if (bezierPointsListTracker >= bezierPoints.Count - 1)
         {
-            Debug.Log("Calculate B");
-            return bezierPoints[bezierPoints.Count - 1].y;
+            //If not, the zero is the y of the closest point
+            //Debug.Log("Calculate A");
+            return camBezierPoints[0].y;
+        }
+        else if (bezierPointsListTracker >= camBezierPoints.Count - 1)
+        {
+            //Debug.Log("Calculate B");
+            return camBezierPoints[camBezierPoints.Count - 1].y;
         }
         else
-        {//If so, perform equation
-            Debug.Log("Calculate C");
-            Debug.Log(a_x_pos);
-            Debug.Log(b_x_pos);
-            float test = slope * Mathf.Cos(car_pos.x * Mathf.PI / b_x_pos) + constant;
-            Debug.Log(test);
-            Debug.Log("Calculate C DONE");
+        {
+            //If so, perform equation
+            //Debug.Log("Calculate C");
+            float currPosOnCurve = slope * Mathf.Cos(car_pos.x * Mathf.PI / currBpoint.x) + constant;
+            //Debug.Log(currPosOnCurve);
+            //Debug.Log("Calculate C DONE");
       
-            return test;
-            
+            return currPosOnCurve;
         }
     }
 
@@ -155,11 +170,11 @@ public class CameraHandler : MonoBehaviour
         //If the vehicle was able to be grabbed earlier in the script
         if (vehicle)
         {
-            //We check if the car is NOT between the current 2 points
-            if (!(a_x_pos <= vehicle.transform.position.x && vehicle.transform.position.x <= b_x_pos)) 
+            // We check if the car is NOT between the current 2 points
+            if (!(currApoint.x <= vehicle.transform.position.x && vehicle.transform.position.x <= currBpoint.x)) 
             {
                 //If so, we either increment or decrement the tracker...
-                if (b_x_pos > vehicle.transform.position.x)
+                if (currBpoint.x > vehicle.transform.position.x)
                 {
                     bezierPointsListTracker -= 1;
                 }
@@ -170,73 +185,125 @@ public class CameraHandler : MonoBehaviour
 
                 //Check to make sure that the car is within range of the points
                 if (bezierPointsListTracker < 0)
-                {//If not, the zero is the y of the closest point
-                    a_x_pos = float.NegativeInfinity;
-                    b_x_pos = bezierPoints[0].x;
-                    Debug.Log("A RETURN");
-                    return bezierPoints[0].y;
-                }
-                else if(bezierPointsListTracker >= bezierPoints.Count - 1)
                 {
-                    a_x_pos = bezierPoints[bezierPoints.Count - 1].x;
-                    b_x_pos = float.PositiveInfinity;
-                    Debug.Log("B RETURN");
-                    return bezierPoints[bezierPoints.Count - 1].y;
+                    //If not, the zero is the y of the closest point
+                    currApoint.x = float.NegativeInfinity;
+                    currBpoint.x = camBezierPoints[0].x;
+                    //Debug.Log("A RETURN");
+                    return camBezierPoints[0].y;
+                }
+                else if(bezierPointsListTracker >= camBezierPoints.Count - 1)
+                {
+                    currApoint.x = camBezierPoints[camBezierPoints.Count - 1].x;
+                    currBpoint.x = float.PositiveInfinity;
+                    //Debug.Log("B RETURN");
+                    return camBezierPoints[camBezierPoints.Count - 1].y;
                 }
                 else
                 {
                     //And then form a new equation for 0!
-                    MakeEquation(bezierPoints[bezierPointsListTracker], bezierPoints[bezierPointsListTracker + 1]);
+                    MakeEquation(camBezierPoints[bezierPointsListTracker], camBezierPoints[bezierPointsListTracker + 1]);
                 }
             }
             //Then we calculate and return a 'zero' point using the current (whether new or old) equation
-            Debug.Log("C RETURN");
+            //Debug.Log("C RETURN");
             return CalculateZero(vehicle.transform.position);
         }
+
         //If no vehicle was grabbed, defaults to 0
         Debug.Log("D RETURN");
         return 0.0f;
     }
 
-    // Additional value added (technically subtracted) to the zero point
-    public float cameraForgiveness;
-
-    // Each value = 1/6 of the distance from the ceenter to the screen edge, + is to the left and - is to the right
-    public float cameraSixthOffset = 2;
-
-    // Modified fixedUpdate() to work with the Zero point code
     void FixedUpdate()
     {
-        // if vehicle  is found
+        // Check if vehicle is found
         if (vehicle)
         {
+            Vector3 vehiclePos = vehicle.transform.position;
 
-            // Calculate the current velocity as a percentage of the velocityRange
-            float velocityPercent = Mathf.InverseLerp(velocityRange.x, velocityRange.y, vehicleRb.velocity.magnitude);
+            // Calculate the zero position based on the car's y position
+            currZeroPos = GetCurrentZero();
+            //Debug.Log("currZeroPos " + currZeroPos);
 
-            //Set zPos based on car's Y position from the zero, and the camera's FOV (60 in this case) divided by 2 (so, 30)
-            float testa = (GetCurrentZero() - cameraForgiveness);
-            float test = -(Mathf.Abs((vehicle.transform.position.y - testa)) / Mathf.Tan(30 * Mathf.Deg2Rad));
-            //Debug.Log("Actual Math: " + test);
-            //Debug.Log("Get Current Zero: " + testa);
-            //Debug.Log("Points! " + bezierPointsListTracker);
-            float zPos = Mathf.Max(zPosRange.y,Mathf.Min(zPosRange.x, test));
+            // << VERT CAMERA SHIFT >>
+            // Calculate the camera shift based on the difference between car's y position and zero position
+            float newYOffset = -(MathF.Abs(vehiclePos.y - currZeroPos) * 0.25f); // get the midpoint between the two
+            newYOffset = Mathf.Clamp(newYOffset, yPosRange.x, yPosRange.y);
 
-            // Set camera x to car x, offset by cameraSixthOffset
-            float cameraShift = Mathf.Abs(vehicle.transform.position.y - testa);
-            //camerashift gets divided by 4.5 and multiplied by 8 to match x:y ratio, then multiplied by cameraSixthOffset/6. below is that, but simplified
-            cameraShift = cameraShift * cameraSixthOffset * 8 / 27;
-            Debug.Log("camShift: " + cameraShift);
-            float xPos = Mathf.Min(xPosRange.y, Mathf.Max(xPosRange.x, (vehicle.transform.position.x + cameraShift)));
+            // << HORZ CAMERA SHIFT >>
+            // Calculate the x position of the camera based on the car's x position and camera shift
+            float newXOffset = newYOffset * cameraSixthOffset * (8 / 27);
+            newXOffset = Mathf.Clamp(newXOffset, xPosRange.x, xPosRange.y);
 
-            // Set camera y to car y
-            float yPos = Mathf.Min(yPosRange.y, Mathf.Max(yPosRange.x, vehicle.transform.position.y));
+            // << CAMERA ZOOM >>
+            // Calculate the z position of the camera based on the car's y position and camera's FOV
+            float newZOffset = -(Mathf.Abs((vehiclePos.y - currZeroPos)) / Mathf.Tan(30 * Mathf.Deg2Rad));
+            newZOffset = Mathf.Clamp(newZOffset, zPosRange.x, zPosRange.y);
 
-            // Update the camera's position with the new x/y/z-position
-            currOffset = new Vector3(xPos, yPos, zPos); //currOffset stores the goal position (at least, here it does...)
-            transform.position = Vector3.Lerp(transform.position, currOffset, camSpeed * Time.deltaTime);
-
+            // [[ LERP CAMERA ]]
+            // Lerp the offset / zoom individually
+            currCamOffset.x = Mathf.Lerp(currCamOffset.x, vehiclePos.x + newXOffset, offsetAdjustSpeed.x * Time.fixedDeltaTime);
+            currCamOffset.y = Mathf.Lerp(currCamOffset.y, vehiclePos.y + newYOffset, offsetAdjustSpeed.y * Time.fixedDeltaTime);
+            currCamOffset.z = Mathf.Lerp(currCamOffset.z, newZOffset, offsetAdjustSpeed.z * Time.deltaTime);
+            
+            // Lerp the camera position to the specific offset ^^
+            transform.position = Vector3.Lerp(transform.position, currCamOffset, camSpeed * Time.fixedDeltaTime);
         }
     }
+
+
+    private void OnDrawGizmos()
+    {
+
+        Vector3 vehiclePos = vehicle.transform.position;
+
+        Gizmos.color = Color.black;
+
+        // << CURR ZERO LINE FOR CHUNK >>
+        Gizmos.DrawLine(new Vector3(currApoint.x, currZeroPos, 0), new Vector3(currBpoint.x, currZeroPos, 0));
+
+        // << CURR ZERO LINE FROM TRUCK Y >>
+        Gizmos.DrawLine(new Vector3(vehiclePos.x, currZeroPos, 0), new Vector3(vehiclePos.x, vehiclePos.y, 0));
+
+
+        // << CURR HORZ OFFSET >>
+        Gizmos.DrawCube(new Vector3(vehiclePos.x, currCamOffset.y, 0), Vector3.one * 25);
+        Gizmos.DrawCube(new Vector3(currCamOffset.x, vehiclePos.y, 0), Vector3.one * 25);
+
+
+        // << SHOW ALL CAM BEZIER POINTS >>
+        for (int i = 0; i < camBezierPoints.Count; i++)
+        {
+            if (i == bezierPointsListTracker) { Gizmos.color = Color.red; }
+            else if (i == bezierPointsListTracker + 1) { Gizmos.color = Color.blue; }
+            else { Gizmos.color = Color.black; }
+
+            Gizmos.DrawSphere(camBezierPoints[i], 50);
+
+
+            // << ZERO POINT >>
+            // Calculate the position of the zero point at each bezier point
+            float zeroPoint = CalculateZero(vehicle.transform.position);
+
+            // Set the color for the Gizmos line based on the position of the zero point
+            if (zeroPoint > 0f)
+            {
+                Gizmos.color = Color.green; // Zero point above y-axis
+            }
+            else if (zeroPoint < 0f)
+            {
+                Gizmos.color = Color.red; // Zero point below y-axis
+            }
+            else
+            {
+                Gizmos.color = Color.yellow; // Zero point at y-axis
+            }
+
+            // Draw a line from the bezier point to the zero point
+            Gizmos.DrawLine(camBezierPoints[i], new Vector3(camBezierPoints[i].x, zeroPoint, 0f));
+        }
+    }
+
 
 }
