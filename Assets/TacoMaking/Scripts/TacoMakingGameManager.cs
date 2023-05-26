5b04ae14763369273ee73335bdf4d7643149308d
Theo Casey <theocasey11@gmail.com>
2023-05-25 22:09:29 -0700

    perfect taco

--- Assets/TacoMaking/Scripts/TacoMakingGameManager.cs
+++ Assets/TacoMaking/Scripts/TacoMakingGameManager.cs
@@ -207,11 +207,11 @@ public class TacoMakingGameManager : MonoBehaviour
 
             if (score == SUBMIT_TACO_SCORE.COMBO)
             {
-                submissionTaco.PlayPerfectAnim();
+                submissionTaco.PlayComboAnim();
             }
             else if (score == SUBMIT_TACO_SCORE.PERFECT)
             {
-                submissionTaco.PlayComboAnim();
+                submissionTaco.PlayPerfectAnim();
             }
             else
             {
